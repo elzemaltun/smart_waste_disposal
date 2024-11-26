69c4b4e2996024e3b0e2e2fc77cf710344b80cf5
elzemaltun <elzemaltunn@gmail.com>
2024-11-26 07:17:15 +0100

    added test cases to main sketch.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include "blink_led_task.h"
 #include "scheduler.h"
+#include "pir_sensor.h"
+#include "sonar_sensor.h"
 
 Scheduler sched;
 BlinkTask* blinkTask;
@@ -10,26 +12,38 @@ bool userDetected = true;
 
 void setup() {
   Serial.begin(9600);
-  sched.init(50);
+  /* sched.init(50);
   blinkTask = new BlinkTask(13, 12); // Red LED on pin 13, Green LED on pin 12
   blinkTask->init(100); // Period set to 100ms 
-  sched.addTask(blinkTask);
+  sched.addTask(blinkTask); */
 }
 
 void loop() {
   // Example: Update states based on sensor data
-  if (errorDetected) {
+/*   if (errorDetected) {
     blinkTask->setState(BlinkTask::RED_ON);
-    Serial.print("this is red state if statement");
   } else if (userDetected) {
     blinkTask->setState(BlinkTask::GREEN_ON);
-    Serial.print("this is green state if statement");
   } else {
     blinkTask->setState(BlinkTask::OFF);
-    Serial.print("this is off state if statement");
   }
-
   sched.schedule();
+ */
+
+  sonarSensorCheck(); 
+  pirSensorCheck(); 
+  
 }
 
+void sonarSensorCheck() {
+  SonarSensor sonarSensor;
+  sonarSensor.init(2, 3);
+  Serial.println(sonarSensor.checkWasteLevel(2, 3));
+  //Serial.println(sonarSensor.getDistance(2, 3));
+}
 
+void pirSensorCheck() {
+  PirSensor pirSensor;
+  pirSensor.init(7);
+  Serial.println(pirSensor.isMotionDetected(7)); 
+}
